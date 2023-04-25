fbcfa331225053bc14e2345bccb70a71b89d9682
Stacy <stacymuhiap@gmail.com>
2023-04-25 11:46:42 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  *print_alphabet_x10 - Print alphabet 10 times
+  *
+  *Return: Always 0 (Success)
+  */
+void print_alphabet_x10(void)
+{
+	int n;
+	int l;
+
+	for (n = 1; n <= 10 ; n++)
+	{
+		for (l = 97; l <= 122; l++)
+		{
+			_putchar(l);
+		}
+		_putchar('\n')
+	}
+}
