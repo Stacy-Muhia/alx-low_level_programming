ea7cd8cec673a10c28c28e41bac601f10b1b11b4
Stacy <stacymuhiap@gmail.com>
2023-04-26 19:28:23 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description - prints the first 50 Fibonacci numbers starting with 1 and 2
+  *
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int count
+		unsigned long - x = 0 
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,31 +1,23 @@
 #include "main.h"
 /**
-  *jack_bauer - prints every minute of the day
-  *Return: ...
+  *jack_bauer - prints every minute of the day of from 00:00 to 23:59.
+  *
+  *Return: void
   */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int h, m;
 
-	for (a = 0; a <= 2; a++)
+	for (h = 0; h < 24; h++)
 	{
-		for (b = 0; b <= 3; b++)
+		for (m = 0; m < 60; m++)
 		{
-			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-			{
-				for (c = 0; c <= 5; c++)
-				{
-					for (d = 0; d <= 9; d++)
-					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(58);
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-					}
-				}
-			}
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
