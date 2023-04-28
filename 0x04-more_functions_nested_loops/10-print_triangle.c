766b6d922d9d4389ceddcfb3560092daf3000128
Stacy <stacymuhiap@gmail.com>
2023-04-28 13:47:13 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,18 +6,25 @@
   */
 void print_triangle(int size)
 {
-	int x, y;
-
-	for (x = 1; x <= size; x++)
+	if (size <= 0)
 	{
-		for (y = x; y <= size; y++)
-		{
-			_putchar(' ');
-		}
-		for (y = 1; y <= x; y++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+		int x, y;
+
+		for (x = 1; x <= size; x++)
+		{
+			for (y = x; y <= size; y++)
+			{
+				_putchar(' ');
+			}
+			for (y = 1; y <= x; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
