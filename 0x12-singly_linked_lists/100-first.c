9d1166d7dc930797c8dcf6ea710dc254498d63c4
Stacy <stacymuhiap@gmail.com>
2023-06-02 15:17:28 -0700

    advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void before_main(void)  __attribute__((constructor));
+/**
+  *before_main - function executed before the main function
+  */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
