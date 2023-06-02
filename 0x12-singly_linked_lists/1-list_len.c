5c6ae76ecb749de69821c99ebcd32bceb0f8596f
Stacy <stacymuhiap@gmail.com>
2023-06-02 11:49:15 -0700

    alx tasks

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+  *list_len - function that returns the number of elements
+  *in a linked list_t list.
+  *@h: The head of the list
+  *
+  *Return: Number of elements of te list.
+  */
+size_t list_len(const list_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		x++;
+		h = h->next;
+	}
+	return (x);
+}
