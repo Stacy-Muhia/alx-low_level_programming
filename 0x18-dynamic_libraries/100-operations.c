f896d4ce4d79a7ae43d829bed2bad2459c26b96d
Stacy-Muhia <stacymuhiap@gmail.com>
2023-08-21 11:02:01 -0700

    test drive

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * sum - add numbers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - functions to  subtract numbers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: difference
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+/**
+ * div - functions to divide numbers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: the divident
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+/**
+ * mul - functions to multiplies numbers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: the product
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * mod - returns the remainder of a number
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: remainder
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
