983187aeb6eb59ac95512e312b66649ddec07088
Stacy-Muhia <stacymuhiap@gmail.com>
2023-09-08 22:00:21 -0700

    2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,7 +8,7 @@
  * Return:  index at which the key/value pair should
  * be stored in the array of the hash table
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	if (key == NULL || size == 0)
 		return (0);
