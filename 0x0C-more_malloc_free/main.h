e91f9402228d7368b8a9ac116d694bbc7b9f661a
Stacy-Muhia <stacymuhiap@gmail.com>
2024-01-08 04:02:04 -0800

    more malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -14,5 +14,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void errors(void);
 int _strlen(char *s);
 int is_digit(char *s);
+int _putchar(char c);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
+
 
 #endif
